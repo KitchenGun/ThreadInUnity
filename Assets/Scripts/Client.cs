90b21afd63a138dd8706a70e29753ca149c0dfd0
KitchenGun <kitchengun124@gmail.com>
2021-02-01 02:47:59 +0900

    예제

---
+++ Assets/Scripts/Client.cs
@@ -0,0 +1,63 @@
+﻿/*
+ * 클라이언트
+ * 서버에 데이타를 요청하고 응답받아 UI에 표시
+*/
+
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Client : MonoBehaviour 
+{
+    //Inspector
+    public Text textUI;         //데이타를 표시할 UI
+    //Concrete classes
+    private SocketLib socket;   //서버와 통신하는 소켓라이브러리
+    private Postbox postbox;    //메세지 큐를 관리하는 우편함
+
+	void Awake () 
+    {
+        socket = new SocketLib();
+        postbox = Postbox.GetInstance;
+        
+        //큐 탐색 시작
+        StartCoroutine(CheckQueue());
+        //데이타 요청 시작
+        RequestData();
+	}
+
+    //서버에 데이타 요청
+    private void RequestData()
+    {
+        socket.Request();
+    }
+
+    //서버에서 응답 받음
+    public void ResponseData(string data)
+    {
+        textUI.text = data;
+    }
+
+    //큐를 주기적으로 탐색
+    private IEnumerator CheckQueue()
+    {
+        //1초 주기로 탐색
+        WaitForSeconds waitSec = new WaitForSeconds(1);
+
+        while (true)
+        {
+            //우편함에서 데이타 꺼내기
+            string data = postbox.GetData();
+
+            //우편함에 데이타가 있는 경우
+            if (!data.Equals(string.Empty))
+            {
+                //데이타로 UI 갱신
+                ResponseData(data);
+                yield break;
+            }
+
+            yield return waitSec;
+        }
+    }
+}
---
+++ Assets/Scripts/SocketLib.cs
@@ -0,0 +1,43 @@
+﻿/*
+ * 서버와 통신하는 소켓 라이브러리 흉내
+*/
+
+using System.Threading;
+
+public class SocketLib
+{
+    //서버 작업 처리 동안 대기할 쓰레드
+    private Thread thread;
+    
+    public SocketLib()
+    {
+        thread = new Thread(new ThreadStart(Run));
+    }
+
+    //서버에 요청
+    public void Request()
+    {
+        thread.Start();
+    }
+
+    //서버 작업 진행
+    void Run()
+    {
+        //서버 작업 시작
+        string result;
+
+        Thread.Sleep(3000);
+
+        //서버 작업 종료
+        result = "This is result data.";
+
+        Response(result);
+    }
+
+    //응답 받음
+    public void Response(string result)
+    {
+        //결과 데이타를 큐에 넣기
+        Postbox.GetInstance.PushData(result);
+    }
+}
